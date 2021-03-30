fde9dd6520cd944ae1a4c052fbc36a9eedd6db80
mdimrannazir <imrannazir1.in@gmail.com>
2021-03-30 14:45:58 +0600

    Banner and Nav first commit

---
+++ src/Header.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import styled from 'styled-components'                                //Installed to use this package(Must NPM Install(npm i styled-components))
+import SearchIcon from '@material-ui/icons/Search';                  //Importing Material Icon to use icons later on(Must NPM Install(npm i @material-ui/core && npm i @material-ui/icons)-using cmd)
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';  
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo>
+                <img src={"https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"}/>
+            </HeaderLogo>
+
+            <HeaderOptionAddress>
+                <LocationOnIcon /> 
+                <HeaderOption>
+                <OptionLineOne>Hello</OptionLineOne>
+                <OptionLineTwo>Select Your Address</OptionLineTwo>
+                </HeaderOption>
+            </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+                
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello, Imran</OptionLineOne>
+                    <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+
+
+                
+                <HeaderOptionCart>
+                <Link to="/cart">
+                    <ShoppingBasketIcon />
+                    <CartCount>3</CartCount>
+                </Link>
+                </HeaderOptionCart>
+                
+                
+
+            </HeaderNavItems>
+        </Container>
+    )
+}
+
+
+
+export default Header
+//Creating components here
+const Container = styled.div`
+    height: 60px;
+    background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+`
+
+const HeaderLogo = styled.div`
+    img {
+        width: 100px;
+        margin-left: 11px;
+    }
+`
+
+const HeaderOptionAddress = styled.div`
+    padding: 0px 0px 0px 9px;
+    display: flex;
+    align-items: center;
+`
+
+const OptionLineOne = styled.div`
+
+`
+
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`
+const HeaderSearch = styled.div`
+    display: flex;
+    flex-grow: 1;
+    height: 40px;
+    border-radius: 4px;
+    overflow: hidden;
+    margin-left:5px;
+    background-color: white;
+    : focus-within {
+        box-shadow: 0 0 0 3px #F90;
+    }
+
+    
+`
+
+const HeaderSearchInput = styled.input`
+    flex-grow: 1;
+    border: 0;
+    : focus {
+        outline: none;
+    }
+`
+
+const HeaderSearchIconContainer = styled.div`
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const HeaderNavItems = styled.div`
+    display: flex;
+`
+const HeaderOption = styled.div`
+padding: 10px 9px 10px 9px;
+`
+const HeaderOptionCart = styled.div`
+display:flex;
+a {
+        display:flex;
+        align-items: center;
+        padding: 0px 9px 0px 0px;
+        color: white;
+        text-decoration: none;
+    }
+    
+    
+`
+const CartCount = styled.div`
+padding: 0px 0px 0px 4px;
+`
